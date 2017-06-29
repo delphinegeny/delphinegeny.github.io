51acdad7a42ab72a9d66a88377bffc99347e314d
delphinegeny <delphine.geny@gmail.com>
2017-06-29 19:21:30 +0200

    primo commit

---
+++ NEWscripts/NEWfile.js
@@ -0,0 +1,77 @@
+$(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("." + selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300);
+
+  });
+});
+
+
+$(function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href");if($(window).scrollTop()!="0"){$(this).fadeIn("slow")}var scrollDiv=$(this);$(window).scroll(function(){if($(window).scrollTop()=="0"){$(scrollDiv).fadeOut("slow")}else{$(scrollDiv).fadeIn("slow")}});$(this).click(function(){$("html, body").animate({scrollTop:0},"slow")})}});
+
+$(function(){
+	$("#toTop").scrollToTop();
+});
+
+
+$(function() {
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
+
+$(function() {
+// Get the modal
+var modal = document.getElementById( "myModal2");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close2")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
