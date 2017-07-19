8e3422c9b95bfc6df3a8244662232892a8323928
delphinegeny <delphine.geny@gmail.com>
2017-07-19 12:22:23 +0200

    19/07

--- NEWscripts/NEWfile.js
+++ NEWscripts/NEWfile.js
@@ -12,6 +12,14 @@ $(function() {
   });
 });
 
+function readmore() {
+    var x = document.getElementById('Read');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
 
 $(function(){$.fn.scrollToTop=function(){$(this).hide().removeAttr("href");if($(window).scrollTop()!="0"){$(this).fadeIn("slow")}var scrollDiv=$(this);$(window).scroll(function(){if($(window).scrollTop()=="0"){$(scrollDiv).fadeOut("slow")}else{$(scrollDiv).fadeIn("slow")}});$(this).click(function(){$("html, body").animate({scrollTop:0},"slow")})}});
 
@@ -85,6 +93,34 @@ var btn = document.getElementById("myBtn3");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close3")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+});
+
+$(function() {
+// Get the modal
+var modal = document.getElementById( "myModal4");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn4");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close4")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
